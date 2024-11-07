f17fefc992ced714344aaa31d44759e4caf5abb2
Eduardo Backes <eduardo.programacao@hotmail.com>
2024-11-07 17:22:45 -0300

    att, testar e criar logica para cor dos usuarios

--- src/connection.go
+++ src/connection.go
@@ -3,31 +3,29 @@ package src
 import (
 	"bufio"
 	"fmt"
-	"math/rand/v2"
 	"net"
 	"os"
 )
 
 func Connection() {
-	cores := []string{
-		"\033[91m",  
-		"\033[92m",  
-		"\033[93m",  
-		"\033[94m",  
-		"\033[95m",  
-		"\033[96m",  
-	}
+	// red := color.New(color.FgRed).Add(color.Underline).SprintFunc()
+	// green := color.New(color.FgGreen).Add(color.Underline).SprintFunc()
+	// blue := color.New(color.FgBlue).Add(color.Underline).SprintFunc()
+	// yellow := color.New(color.FgYellow).Add(color.Underline).SprintFunc()
+	// white := color.New(color.FgWhite).Add(color.Underline).SprintFunc()
+	// cores := []interface{}{
+	// 	red,green,blue,yellow,white,
+	// }
 	conexao, err := net.Dial("tcp", "192.168.1.113:8081")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(3)
 	}
-
 	nome := ""
 	if nome == "" {
 		nome = User()
 	}
-	ran := cores[rand.IntN(len(cores))]
+	// ran := cores[rand.IntN(len(cores))]
 	for {
 		leitor := bufio.NewReader(os.Stdin)
 		texto, textoErr := leitor.ReadString('\n')
@@ -36,14 +34,7 @@ func Connection() {
 			os.Exit(3)
 		}
 
-		fmt.Fprintf(conexao, ran+nome+": "+"\033[0m"+texto+"\n")
+		fmt.Fprintf(conexao, nome+": "+texto+"\n")
 		fmt.Println("")
-
-		// _, err3 := bufio.NewReader(conexao).ReadString('\n')
-		// if err3 != nil {
-		// 	fmt.Println(err3)
-		// 	os.Exit(3)
-		// }
-		
 	}
 }
