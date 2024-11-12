a9ea895fe59b8298e6768e56a8292f33d65f72f4
Eduardo Backes <128044560+EduardoBackesdev@users.noreply.github.com>
2024-11-11 22:23:18 -0300

    att

--- src/server.go
+++ src/server.go
@@ -17,16 +17,5 @@ func Server() {
 		mes := <-chMes
 		con := <-ch
 		Hub(mes, con)
-		//Hub(mes, conn)
-		// fmt.Println("Entrou para digitar")
-		// conexao := <-ch
-		// leitor := bufio.NewReader(os.Stdin)
-		// texto, textoErr := leitor.ReadString('\n')
-		// if textoErr != nil {
-		// 	fmt.Println(textoErr)
-		// 	os.Exit(3)
-		// }
-		// fmt.Fprintf(conexao, texto+"\n")
-		// fmt.Println("")
 	}
 }
